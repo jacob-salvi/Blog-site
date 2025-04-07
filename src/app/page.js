3110ccf3a90cc3b923ff69f5d1b62cb3f801ee7a
jacob-salvi <salvijacob774@gmail.com>
2025-04-07 14:03:41 +0530

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className="text-2xl top-1/2 left-1/2"> hello , welcome to my blog</h1>
+   </main>
+  );
+}
