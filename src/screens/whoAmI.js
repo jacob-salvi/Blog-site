98422dc53aaf276cfab4b27191daeacbeb25e304
jacob-salvi <salvijacob774@gmail.com>
2025-06-13 14:30:18 +0530

    solved eslint problem

--- src/screens/whoAmI.js
+++ src/screens/whoAmI.js
@@ -31,11 +31,11 @@ const whoAmI = () => {
                 </div>
             </div>
             <div className='bioofme  max-w-[80%] text-[1.3rem] tracking-[-4%] leading-[2rem] font-[Gabarito] font-light'>
-                <p>I’m Jacob Salvi — someone who’s still figuring things out. Mostly about design, programming, faith, and a whole lot of overthinking.I’m 19 (soon turning 20), and I don’t really consider myself a professional in anything — but definitely a learner.<br/>
-I’m curious about building and designing creative things, and every day I try to learn something new that helps me make better stuff.
-Since finishing my diploma, I’ve been freelancing — and it’s going pretty well. I get to work on the design side professionally, and in my free time, I dive into code. Programming helps me bring my own designs to life.
-Whenever I feel stressed, I either ride my bike or take long walks with my IEMs on.Lately, I’ve been putting more of my thoughts on paper — writing has helped me think more clearly.
-I’m also trying to build a healthier relationship with God. I fail, I fall short, I sin — but I try, and I ask for forgiveness.
+                <p>I'm Jacob Salvi — someone who's still figuring things out. Mostly about design, programming, faith, and a whole lot of overthinking.I'm 19 (soon turning 20), and I don't really consider myself a professional in anything — but definitely a learner.<br/>
+I'm curious about building and designing creative things, and every day I try to learn something new that helps me make better stuff.
+Since finishing my diploma, I've been freelancing — and it's going pretty well. I get to work on the design side professionally, and in my free time, I dive into code. Programming helps me bring my own designs to life.
+Whenever I feel stressed, I either ride my bike or take long walks with my IEMs on.Lately, I've been putting more of my thoughts on paper — writing has helped me think more clearly.
+I'm also trying to build a healthier relationship with God. I fail, I fall short, I sin — but I try, and I ask for forgiveness.
 I love capturing moments — photos I can look back at, and remember what life felt like.</p>
             </div>
         </div>
