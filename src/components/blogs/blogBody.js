26911ee3739e9ff446a2905e1c98a2d8bc870ba7
jacob-salvi <salvijacob774@gmail.com>
2026-06-04 12:35:18 +0530

    fix: build errors

--- src/components/blogs/blogBody.js
+++ src/components/blogs/blogBody.js
@@ -3,15 +3,11 @@ import { MDXRemote } from 'next-mdx-remote'
 import React from 'react'
 import { Gabarito, DM_Serif_Text } from 'next/font/google'
 import { ReactLenis, useLenis } from 'lenis/react'
-import { useRouter } from 'next/navigation'
 
 
-const blogBody = ({content}) => {
-    const router = useRouter();
 
-  const handleBack = () => {
-    window.history.length > 1 ? router.back() : router.push('/')
-  }
+const BlogBody = ({content}) => {
+
 
   return (
     <div className='w-full  my-14  px-3 md:px-0  flex flex-col items-center gap-10 '>
@@ -31,5 +27,5 @@ const blogBody = ({content}) => {
   )
 }
 
-export default blogBody
+export default BlogBody
 
